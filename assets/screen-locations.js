064ef23d5a29dc9d15bf5707adb78c56164ad6f8
Nicholas Connelly <nicholas_connelly@icloud.com>
2026-05-12 10:56:44 -0600

    Initial: single-page sales-process rebuild staged at reach3.reachscreens.ca

---
+++ assets/screen-locations.js
@@ -0,0 +1,235 @@
+window.screenLocations = [
+  {
+    "id": 1,
+    "name": "Katchai Cafe",
+    "address": "C-5730 44 Street, Lloydminster, AB T9V 0B6",
+    "lng": -110.029295,
+    "lat": 53.2785304
+  },
+  {
+    "id": 2,
+    "name": "CL Therapy",
+    "address": "4161 70 Avenue, Lloydminster, AB T9V 3L9",
+    "lng": -110.04734,
+    "lat": 53.27302
+  },
+  {
+    "id": 3,
+    "name": "Cuts by Jord",
+    "address": "5710 44 Street, Lloydminster, AB T9V 0B6",
+    "lng": -110.02637,
+    "lat": 53.27855
+  },
+  {
+    "id": 4,
+    "name": "Superior Water",
+    "address": "4305B 57 Avenue, Lloydminster, AB T9V 1Y4",
+    "lng": -110.0251,
+    "lat": 53.27726
+  },
+  {
+    "id": 5,
+    "name": "Mr. Sparkles",
+    "address": "6201 43 Street, Lloydminster, AB T9V 2W9",
+    "lng": -110.03456,
+    "lat": 53.27635
+  },
+  {
+    "id": 6,
+    "name": "3 Guys Truck Wash",
+    "address": "4807 40 Avenue, Lloydminster, SK S9V 1Y8",
+    "lng": -109.97811,
+    "lat": 53.28196
+  },
+  {
+    "id": 7,
+    "name": "Truck Zone",
+    "address": "5205 65 Street, Lloydminster, AB T9V 2E8",
+    "lng": -110.01431,
+    "lat": 53.30287
+  },
+  {
+    "id": 8,
+    "name": "Alicia's Apothecary",
+    "address": "101-1724 50 Avenue, Lloydminster, AB T9V 0Y1",
+    "lng": -110.0059,
+    "lat": 53.26645
+  },
+  {
+    "id": 9,
+    "name": "Fountain Tire",
+    "address": "5110 63 Avenue, Lloydminster, AB T9V 2E6",
+    "lng": -110.03752,
+    "lat": 53.28594
+  },
+  {
+    "id": 10,
+    "name": "Neighbours Pub",
+    "address": "3-5601 31 Street, Lloydminster, AB T9V 2H3",
+    "lng": -110.02223,
+    "lat": 53.26681
+  },
+  {
+    "id": 11,
+    "name": "Supplement King",
+    "address": "105-4100 70 Avenue, Lloydminster, AB T9V 2X3",
+    "lng": -110.04869,
+    "lat": 53.27435
+  },
+  {
+    "id": 12,
+    "name": "In2Golf",
+    "address": "6202 48 Street, Lloydminster, AB T9V 2G1",
+    "lng": -110.03472,
+    "lat": 53.28257
+  },
+  {
+    "id": 13,
+    "name": "Sunrise Pharmacy",
+    "address": "102-5001 18 Street, Lloydminster, AB T9V 2G7",
+    "lng": -110.00628,
+    "lat": 53.25307
+  },
+  {
+    "id": 14,
+    "name": "Marc'd Up Tattoos",
+    "address": "12-1804 50 Avenue, Lloydminster, AB T9V 2W7",
+    "lng": -110.00588,
+    "lat": 53.26445
+  },
+  {
+    "id": 15,
+    "name": "Nouveau Lasers",
+    "address": "1429B 50 Avenue, Lloydminster, SK S9V 2K1",
+    "lng": -110.005488,
+    "lat": 53.2552228
+  },
+  {
+    "id": 16,
+    "name": "Aveiro Sleep",
+    "address": "101-5101 48 Street, Lloydminster, AB T9V 0H9",
+    "lng": -110.0084442,
+    "lat": 53.2814466
+  },
+  {
+    "id": 17,
+    "name": "Station Auto",
+    "address": "5013 49 Avenue, Lloydminster, SK S9V 0T8",
+    "lng": -110.0029182,
+    "lat": 53.2841821
+  },
+  {
+    "id": 18,
+    "name": "Border City Dental",
+    "address": "4804 50 Street, Lloydminster, SK S9V 0M9",
+    "lng": -110.000823,
+    "lat": 53.2818884
+  },
+  {
+    "id": 19,
+    "name": "Diamond Auto",
+    "address": "4634 44 Street, Lloydminster, SK S9V 0G4",
+    "lng": -109.9971602,
+    "lat": 53.2783075
+  },
+  {
+    "id": 20,
+    "name": "Lloyd Hi-Quality Auto",
+    "address": "2709 50 Avenue, Lloydminster, SK S9V 2K1",
+    "lng": -110.0046855,
+    "lat": 53.2622457
+  },
+  {
+    "id": 21,
+    "name": "Sage and Soul",
+    "address": "1-2801 50 Avenue, Lloydminster, SK S9V 2A8",
+    "lng": -110.0039764,
+    "lat": 53.2628901
+  },
+  {
+    "id": 22,
+    "name": "The Sticks",
+    "address": "5704 44 Street, Lloydminster, AB T9V 0B6",
+    "lng": -110.0248362,
+    "lat": 53.2788003
+  },
+  {
+    "id": 23,
+    "name": "Imagine Laser Works",
+    "address": "106-5101 48 Street, Lloydminster, AB T9V 2G5",
+    "lng": -110.0084442,
+    "lat": 53.2814466
+  },
+  {
+    "id": 24,
+    "name": "Superior Water (Coin-Op)",
+    "address": "3245 50 Avenue, Lloydminster, SK S9V 0N8",
+    "lng": -110.0054804,
+    "lat": 53.2507699
+  },
+  {
+    "id": 25,
+    "name": "Hotsul's Ukrainian Cuisine",
+    "address": "3314 50 Avenue, Lloydminster, AB T9V 0R6",
+    "lng": -110.0064709,
+    "lat": 53.2687235
+  },
+  {
+    "id": 26,
+    "name": "Tasty K's",
+    "address": "5008A 39 Street, Lloydminster, AB T9V 2Y8",
+    "lng": -110.0069965,
+    "lat": 53.2734142
+  },
+  {
+    "id": 27,
+    "name": "Viking Strength",
+    "address": "4203 70 Avenue, Lloydminster, AB T9V 3L9",
+    "lng": -110.046947,
+    "lat": 53.2752662
+  },
+  {
+    "id": 28,
+    "name": "Cheers Live",
+    "address": "5501 44 Street, Lloydminster, AB T9V 2H4",
+    "lng": -110.0179997,
+    "lat": 53.2770657,
+    "screens": 2
+  },
+  {
+    "id": 29,
+    "name": "Kat Salon",
+    "address": "5019 50 Street, Lloydminster, AB T9V 0L9",
+    "lng": -110.007219,
+    "lat": 53.2831913
+  },
+  {
+    "id": 30,
+    "name": "Astec Safety",
+    "address": "6206 44 Street, Lloydminster, AB T9V 1V9",
+    "lng": -110.034931,
+    "lat": 53.2789847,
+    "screens": 2
+  },
+  {
+    "id": 31,
+    "name": "Lloydminster Honda",
+    "address": "1904 50 Avenue, Lloydminster, AB T9V 2W7",
+    "lng": -110.0067501,
+    "lat": 53.2558581
+  },
+  {
+    "id": 32,
+    "name": "Wilson Registries",
+    "address": "2-1202 50 Avenue, Lloydminster, AB T9V 0Y1",
+    "lng": -110.0067158,
+    "lat": 53.2494939
+  },
+  {
+    "id": 33,
+    "name": "World Class Training and Nutrition",
+    "address": "6601 43 Street, Lloydminster, AB T9V 3E8",
+    "lng": -110.0416158,
+    "lat": 53.2763864
+  }
+];
